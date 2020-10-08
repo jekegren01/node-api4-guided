a262c4302756a88843a93e6ea234d4505c42ac1a
Jacob Ekegren <JacobEkegren@MacBook-Pro.local>
2020-10-07 17:47:12 -0700

    changing env variable

--- welcome/welcome-router.js
+++ welcome/welcome-router.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 router.get("/", (req, res) => {
 	res.status(200).json({
-		message: `Welcome ${process.env.COHORT} ${process.env.FUN_FACT}`,
+		message: `Welcome ${process.env.COHORT}`,
 		fact: process.env.FUN_FACT || "I have no fun facts",
 	})
 })
